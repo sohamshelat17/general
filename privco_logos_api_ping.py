ebc155257c37af27c95a5045c36d517cca317ffe
Soham Shelat <sohamshelat@yahoo.com>
2020-04-10 18:41:06 -0400

    Update privco_logos_api_ping.py

--- privco_logos_api_ping.py
+++ privco_logos_api_ping.py
@@ -5,7 +5,8 @@ import requests
 #import urllib2
 
 
-imgurl = ['https://logo.clearbit.com/bizongo.in']
+imgurl = ['https://api.ritekit.com/v1/images/logo?domain=diversifiedrestaurantholdings.com&client_id=73ee63c0a381cd1fa879a5aa7bff70a21ffd1d09129a'
+]
 
 for i in imgurl:
     try:
